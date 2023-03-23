7b20895fefd4a3870eca0ead37e2caee6714a2e0
4621553 <xander.mcgowan@gmail.com>
2023-03-22 22:50:52 -0500

    finalised trans code, finshed resume page

--- index.buttons.js
+++ index.buttons.js
@@ -1,91 +1,14 @@
-function fadeOutEffectContact() {
+
+
+function fadeOutEffect(page){
   var fadeTarget = document.getElementById("body");
-  var fadeEffect = setInterval(function () {
-      if (!fadeTarget.style.opacity) {
-          fadeTarget.style.opacity = 1;
-      }
-      if (fadeTarget.style.opacity > 0.034) {
-          fadeTarget.style.opacity -= 0.017;
-      } else {
-          clearInterval(fadeEffect);
-      }
-  }, 0);
+  fadeTarget.classList.add('fadeOut')
 
 
   setTimeout(() => {
-    window.location.href = './contact.html';
+    window.location.href = `./${page}.html`;
   }, "200");
-
 }
-
-// function fadeOutEffectAbout() {
-//   var fadeTarget = document.getElementById("body");
-//   var fadeEffect = setInterval(function () {
-//       if (!fadeTarget.style.opacity) {
-//           fadeTarget.style.opacity = 1;
-//       }
-//       if (fadeTarget.style.opacity > 0) {
-//           fadeTarget.style.opacity -= 0.017;
-//       } else {
-//           clearInterval(fadeEffect);
-//       }
-//   }, 0);
-
-//   setTimeout(() => {
-//     window.location.href = './about.html';
-//   }, "200");
-
-// }
-
-// function fadeOutEffectResume() {
-//   var fadeTarget = document.getElementById("body");
-//   var fadeEffect = setInterval(function () {
-//       if (!fadeTarget.style.opacity) {
-//           fadeTarget.style.opacity = 1;
-//       }
-//       if (fadeTarget.style.opacity > 0) {
-//           fadeTarget.style.opacity -= 0.017;
-//       } else {
-//           clearInterval(fadeEffect);
-//       }
-//   }, 0);
-
-//   setTimeout(() => {
-//     window.location.href = './resume.html';
-//   }, "200");
-
-// }
-
-// function fadeOutEffectGallery() {
-//   var fadeTarget = document.getElementById("body");
-//   var fadeEffect = setInterval(function () {
-//       if (!fadeTarget.style.opacity) {
-//           fadeTarget.style.opacity = 1;
-//       }
-//       if (fadeTarget.style.opacity > 0) {
-//           fadeTarget.style.opacity -= 0.017;
-//       } else {
-//           clearInterval(fadeEffect);
-//       }
-//   }, 0);
-
-//   setTimeout(() => {
-//     window.location.href = './gallery.html';
-//   }, "200");
-
-// }
-
-
-
-// function fadeOutEffect(page){
-//   var fadeTarget = document.getElementById("body");
-//   fadeTarget.classList.add('fadeOut')
-
-
-//   setTimeout(() => {
-//     window.location.href = `./${page}.html`;
-//   }, "200");
-// }
 
 
 
