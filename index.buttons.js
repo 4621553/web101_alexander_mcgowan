ddfd4ebfc4b03d248d389df0a6b1feb59ad2329d
4621553 <xander.mcgowan@gmail.com>
2023-03-22 20:48:00 -0500

    dd

--- index.buttons.js
+++ index.buttons.js
@@ -1,4 +1,81 @@
-function fadeOutEffectContact() {
+// function fadeOutEffectContact() {
+//   var fadeTarget = document.getElementById("body");
+//   var fadeEffect = setInterval(function () {
+//       if (!fadeTarget.style.opacity) {
+//           fadeTarget.style.opacity = 1;
+//       }
+//       if (fadeTarget.style.opacity > 0) {
+//           fadeTarget.style.opacity -= 0.017;
+//       } else {
+//           clearInterval(fadeEffect);
+//       }
+//   }, 0);
+
+
+//   setTimeout(() => {
+//     window.location.href = './contact.html';
+//   }, "200");
+
+// }
+
+// function fadeOutEffectAbout() {
+//   var fadeTarget = document.getElementById("body");
+//   var fadeEffect = setInterval(function () {
+//       if (!fadeTarget.style.opacity) {
+//           fadeTarget.style.opacity = 1;
+//       }
+//       if (fadeTarget.style.opacity > 0) {
+//           fadeTarget.style.opacity -= 0.017;
+//       } else {
+//           clearInterval(fadeEffect);
+//       }
+//   }, 0);
+
+//   setTimeout(() => {
+//     window.location.href = './about.html';
+//   }, "200");
+
+// }
+
+// function fadeOutEffectResume() {
+//   var fadeTarget = document.getElementById("body");
+//   var fadeEffect = setInterval(function () {
+//       if (!fadeTarget.style.opacity) {
+//           fadeTarget.style.opacity = 1;
+//       }
+//       if (fadeTarget.style.opacity > 0) {
+//           fadeTarget.style.opacity -= 0.017;
+//       } else {
+//           clearInterval(fadeEffect);
+//       }
+//   }, 0);
+
+//   setTimeout(() => {
+//     window.location.href = './resume.html';
+//   }, "200");
+
+// }
+
+// function fadeOutEffectGallery() {
+//   var fadeTarget = document.getElementById("body");
+//   var fadeEffect = setInterval(function () {
+//       if (!fadeTarget.style.opacity) {
+//           fadeTarget.style.opacity = 1;
+//       }
+//       if (fadeTarget.style.opacity > 0) {
+//           fadeTarget.style.opacity -= 0.017;
+//       } else {
+//           clearInterval(fadeEffect);
+//       }
+//   }, 0);
+
+//   setTimeout(() => {
+//     window.location.href = './gallery.html';
+//   }, "200");
+
+// }
+
+function fadeOutEffect(page){
   var fadeTarget = document.getElementById("body");
   var fadeEffect = setInterval(function () {
       if (!fadeTarget.style.opacity) {
@@ -11,68 +88,9 @@ function fadeOutEffectContact() {
       }
   }, 0);
 
-
   setTimeout(() => {
-    window.location.href = './contact.html';
+    window.location.href = `./${page}.html`;
   }, "200");
-
-}
-
-function fadeOutEffectAbout() {
-  var fadeTarget = document.getElementById("body");
-  var fadeEffect = setInterval(function () {
-      if (!fadeTarget.style.opacity) {
-          fadeTarget.style.opacity = 1;
-      }
-      if (fadeTarget.style.opacity > 0) {
-          fadeTarget.style.opacity -= 0.017;
-      } else {
-          clearInterval(fadeEffect);
-      }
-  }, 0);
-
-  setTimeout(() => {
-    window.location.href = './about.html';
-  }, "200");
-
-}
-
-function fadeOutEffectResume() {
-  var fadeTarget = document.getElementById("body");
-  var fadeEffect = setInterval(function () {
-      if (!fadeTarget.style.opacity) {
-          fadeTarget.style.opacity = 1;
-      }
-      if (fadeTarget.style.opacity > 0) {
-          fadeTarget.style.opacity -= 0.017;
-      } else {
-          clearInterval(fadeEffect);
-      }
-  }, 0);
-
-  setTimeout(() => {
-    window.location.href = './resume.html';
-  }, "200");
-
-}
-
-function fadeOutEffectGallery() {
-  var fadeTarget = document.getElementById("body");
-  var fadeEffect = setInterval(function () {
-      if (!fadeTarget.style.opacity) {
-          fadeTarget.style.opacity = 1;
-      }
-      if (fadeTarget.style.opacity > 0) {
-          fadeTarget.style.opacity -= 0.017;
-      } else {
-          clearInterval(fadeEffect);
-      }
-  }, 0);
-
-  setTimeout(() => {
-    window.location.href = './gallery.html';
-  }, "200");
-
 }
 
 const ready = () => {
